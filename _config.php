d7bd34a7ab3a233a3166a60456a7aa6fbabdfcad
Çağatay İlhan <icagatay930@gmail.com>
2023-04-03 11:46:49 +0300

    Initial commit

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "Anitium"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png"; // Logo
+$contactEmail = "anitium09@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "https://patreon.com/anitium/";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://anitium.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://anitium.cagatayilhan.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
